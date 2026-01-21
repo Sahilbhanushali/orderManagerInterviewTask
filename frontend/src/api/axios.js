2a61347116368ac09e8562218b74ec28b015fe27
Sahil Bhanushali <sahilbhanushali5678@gmail.com>
2026-01-22 03:27:29 +0530

    changedIn axios.js in frntend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: `${process.env.BACKEND_URL}/api`,
     headers: {
         'Content-Type': 'application/json'
     }
